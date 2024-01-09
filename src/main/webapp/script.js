8e5b3c051e0448db7e182a87dfcdc74721ea9a7e
abc <ab@cd>
2024-01-09 07:28:48 +0400

    v0.1

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,96 @@
+function findMessages() {
+    clearList();
+    if(document.getElementById("loginUsername").value==""||document.getElementById("loginPassword").value==""){
+        alert("Username and Password is Required");
+        return;
+    }
+    var message="sdasd";
+    addMessage(message);
+    document.getElementById("loginUsername").value="";
+    document.getElementById("loginPassword").value=""
+}
+
+function addMessage(message){
+    var ul = document.getElementById("messages");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(message));
+    ul.appendChild(li);
+}
+
+function clearList(){
+    var ul = document.getElementById("messages");
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild);
+    }
+}
+
+function registerUser(){
+    if(document.getElementById("username").value==""||document.getElementById("password").value==""){
+        alert("Username and Password is Required");
+        return;
+    }
+    document.getElementById("username").value="";
+    document.getElementById("password").value="";
+}
+
+function sendMessage(){
+    if(document.getElementById("user").value==""||document.getElementById("message").value==""){
+        alert("Target user and Message is Required");
+        return;
+    }
+    document.getElementById("user").value="";
+    document.getElementById("message").value="";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Request-ის გაგზავნა
 // async function register() {
 //     var url = webserverName + servletUrl + '?param1=param1Value' + '&param2=param2Value';
